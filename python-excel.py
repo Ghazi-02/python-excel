112251fce74d9c7d21604f231f5d674b23b84753
Areeb <auhmirza02@gmail.com>
2023-01-13 12:01:05 +0000

    Add coloumn specific price changer function

---
+++ python-excel.py
@@ -0,0 +1,36 @@
+from openpyxl import Workbook,load_workbook
+
+
+#create a workbook object
+#wb = Workbook()
+
+#load existing speadsheet
+
+wb=load_workbook('tyres.xlsx')
+
+#create an active  worksheet
+ws = wb.active
+
+"""
+dic = {}
+x = 1
+while x < 6:
+    size = ws[f"A{x}"].value
+    price = ws[f"B{x}"].value
+    dic.update({size :f"{price}"})
+    x += 1
+print(dic)
+#Returns dictionary dic in form {'Tyres': 'Price', '135 40 14': '15', '155 55 16': '20', '195 55 16': '25', '185 35 18': '30'}
+"""
+def price_changer(file,qty,coloumn): #arg file and coloumn must be strings
+    coloumn_ = ws[coloumn]
+    print(coloumn)
+    for cell in coloumn_:
+        if type(cell.value) == str:
+            cell.value = cell.value
+        else:
+            cell.value = cell.value + qty 
+    wb.save(file)
+
+
+ 
